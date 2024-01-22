3a78b4b798822b1ef563b23d0fdd3eca07d73114
keoni <kimkeonhwi991231@gmail.com>
2024-01-23 02:27:31 +0900

    주문 도메인 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,10 +11,11 @@ import java.util.ArrayList;
 import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@Getter @Setter
+@Getter
+//@Setter //@Setter를 쓰지 말고 비즈니스 메서드를 이요해서
 @DiscriminatorColumn(name = "dtype")
 public abstract class Item {
-
+    //객체지향적으로 생각했을때 데이터를 가지고 있는 쪽에 비즈니스 메서드가 있는게 가장 좋다.
     @Id
     @GeneratedValue
     @Column(name = "item_id")
@@ -26,5 +28,24 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
-    public Item() {    }
+    public Item() {}
+
+    // ======= 비즈니스 로직 ====== //
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int realStock = this.stockQuantity - quantity;
+        if(realStock < 0 ) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = realStock;
+    }
 }
